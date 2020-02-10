d2b9cb89518fb1b6d71347f2906b710be703e15b
GalBrandwine <gal080592@gmail.com>
2020-02-10 22:09:14 +0200

    Added simply version of pysimpleGUI_just_buttons.py

--- driverScorer/pysimpleGUI_just_buttons.py
+++ driverScorer/pysimpleGUI_just_buttons.py
@@ -2,15 +2,19 @@ import PySimpleGUI as sg
 
 sg.theme('DarkAmber')  # Add a touch of color
 LARGEBUTTONSIZE = (24, 12)
+SMALLBUTTONSIZE = (24,1)
+WINDOWWIDTH = 320
+WINDOWHEIGHT = 240
+
 # All the stuff inside your window.
-layout = [[sg.Text('Who drives?',justification='center')],
+layout = [[sg.Text(size=SMALLBUTTONSIZE),sg.Text('Who drives?',justification='center'),sg.Text(size=SMALLBUTTONSIZE)],
             [sg.Button('Gal', key="gal", size=LARGEBUTTONSIZE),
             sg.Button('Rivka', key='rivka',size=LARGEBUTTONSIZE),
             sg.Button('Other', key='other',size=LARGEBUTTONSIZE)]
           ]
 
 # Create the Window
-window = sg.Window('DrivingScorer', layout)
+window = sg.Window('DrivingScorer', layout,size=(WINDOWWIDTH,WINDOWHEIGHT))
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
@@ -22,5 +26,8 @@ while True:
         print(event)
     if event == "other":
         print(event)
+
+    # Update the "output" text element to be the value of "input" element
+    # window['-OUTPUT-'].update(values['-IN-'])
 
 window.close()
