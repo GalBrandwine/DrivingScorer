0eb71d0dbab88ea19628386ccbe52573947c4ca5
GalBrandwine <gal080592@gmail.com>
2020-02-29 13:32:09 +0200

    added smbus_testing.py

--- sensors/IMU/test_imu.py
+++ sensors/IMU/test_imu.py
@@ -1,14 +1,15 @@
 # MPU6050 9-DoF Example Printout
 
-from .mpu9250_i2c import *
+import time
+import sensors.IMU.mpu9250_i2c as mpu9250
 
 time.sleep(1)  # delay necessary to allow mpu9250 to settle
 
 print('recording data')
 while 1:
     try:
-        ax, ay, az, wx, wy, wz = mpu6050_conv()  # read and convert mpu6050 data
-        mx, my, mz = AK8963_conv()  # read and convert AK8963 magnetometer data
+        ax, ay, az, wx, wy, wz = mpu9250.mpu6050_conv()  # read and convert mpu6050 data
+        mx, my, mz = mpu9250.AK8963_conv()  # read and convert AK8963 magnetometer data
     except:
         continue
 
