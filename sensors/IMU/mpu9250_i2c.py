abd3aefccc28d5a4123064ec02d0178094b1b5a2
GalBrandwine <gal080592@gmail.com>
2020-03-01 10:27:37 +0200

    added software I2C support

--- sensors/IMU/mpu9250_i2c.py
+++ sensors/IMU/mpu9250_i2c.py
@@ -148,6 +148,8 @@ AK8963_CNTL = 0x0A
 mag_sens = 4900.0  # magnetometer sensitivity: 4800 uT
 
 # start I2C driver
-bus = smbus.SMBus(1)  # start comm with i2c bus
+hardwareI2c = 1
+softwareI2C = 3
+bus = smbus.SMBus(softwareI2C)  # start comm with i2c bus
 gyro_sens, accel_sens = MPU6050_start()  # instantiate gyro/accel
 AK8963_start()  # instantiate magnetometer
